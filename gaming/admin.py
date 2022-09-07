ee8427e5fcea25445b07192720d39ef4a4c392c7
Bastian Ballmann <balle@codekid.net>
2022-09-07 08:55:45 +0200

    better admin view

--- gaming/admin.py
+++ gaming/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 from .models import Gamesystem, Game
 
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'played', 'finished', 'pub_date')
+    list_filter = ['gamesystems', 'played', 'finished']
+
 admin.site.register(Gamesystem)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
