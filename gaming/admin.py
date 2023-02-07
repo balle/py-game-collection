ddbd5fc321c55174f8e2d9297bd24b77b3ff90ff
Bastian Ballmann <balle@codekid.net>
2023-02-07 18:48:06 +0100

    filter via genre

--- gaming/admin.py
+++ gaming/admin.py
@@ -3,7 +3,7 @@ from .models import Gamesystem, Game, Genre
 
 class GameAdmin(admin.ModelAdmin):
     list_display = ('name', 'played', 'finished', 'created_date', 'started_date', 'finished_date')
-    list_filter = ['gamesystems', 'played', 'finished']
+    list_filter = ['gamesystems', 'genre', 'played', 'finished']
 
 admin.site.register(Gamesystem)
 admin.site.register(Genre)
