19bf23b53ce3c108c851888761dd0869ef8a31ae
Bastian Ballmann <balle@codekid.net>
2023-04-08 09:29:17 +0200

    make games searchable by name in admin interface

--- gaming/admin.py
+++ gaming/admin.py
@@ -4,6 +4,7 @@ from .models import Gamesystem, Game, Genre
 class GameAdmin(admin.ModelAdmin):
     list_display = ('name', 'played', 'finished', 'created_date', 'started_date', 'finished_date')
     list_filter = ['gamesystems', 'genre', 'played', 'finished', 'download_only']
+    search_fields = ['name__icontains']
 
 admin.site.register(Gamesystem)
 admin.site.register(Genre)
