e85a63f299eec1575f88be91177d2956f0d38f2d
Bastian Ballmann <balle@codekid.net>
2025-07-28 15:22:19 +0200

    react game detail view

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -13,11 +13,14 @@ function App() {
   let [games, updateGames] = useState<GameType[]>([]);
   // updateGames(["Crash Bandicoot", "Super Mario", "Wipeout", "Burnout"]);
 
-  const handleSelectedItem = (item: GameType) => {
-    console.log(item);
-  };
+  let base_url = location.origin;
+  base_url = "http://127.0.0.1:8000";
 
-  const apiUrl = "http://127.0.0.1:8000/api/games/?page=1";
+  const apiUrl = `${base_url}/api/games/?page=1`;
+
+  const handleSelectedItem = (item: GameType) => {
+    location.href = `${base_url}/game/${item.id}`;
+  };
 
   useEffect(() => {
     let ajax = new XMLHttpRequest();
