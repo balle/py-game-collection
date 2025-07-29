3718b4544d0fc7743591e326d92331f18f75ef04
Bastian Ballmann <balle@codekid.net>
2025-07-29 19:08:46 +0200

    react component for generic filtering games

---
+++ react-app/src/components/Filter.tsx
@@ -0,0 +1,27 @@
+export type FilterItem = {
+  id: number;
+  name: string;
+};
+
+interface Props {
+  items: FilterItem[];
+  onSelect: (categoryId: string) => void;
+}
+
+const Filter = ({ items, onSelect }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelect(event.target.value)}
+    >
+      <option value="">All</option>
+      {items.map((item) => (
+        <option key={item.id} value={item.id}>
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Filter;
