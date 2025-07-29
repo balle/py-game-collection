93a6004447679c222532d05727436b2d9c6a234a
Bastian Ballmann <balle@codekid.net>
2025-07-29 10:51:07 +0200

    fixtures + api tests

--- gaming/api/urls.py
+++ gaming/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('games/', views.GameViewSet.as_view({'get': 'list'}), name='games'),
+    path('games/', views.GameViewSet.as_view({'get': 'list'}), name='api-games'),
     # path('game/add', views.create_game),
-    path('gamesystems/', views.get_all_gamesystems),
+    path('gamesystems/', views.get_all_gamesystems, name='api-gamesystems'),
     # path('gamesystem/add', views.create_gamesystem),
 ]
--- gaming/tests.py
+++ gaming/tests.py
@@ -5,58 +5,32 @@ from django.urls import reverse
 from .models import Game, Gamesystem, Genre
 
 class GamingTests(TestCase):
-    def setUp(self):
-        self.gamesystem = Gamesystem.objects.create(name="Test Gamesystem")
-        self.genre = Genre.objects.create(name="Ego-Shooter")
-        self.game = Game.objects.create(name="Doom", played=True, finished=False)
-        self.game.gamesystems.add(self.gamesystem)
-        self.game.save()
-
-    def test_game_description(self):
-        self.assertEquals(self.game.description, "")
-        self.game.fetch_description()
-        self.assertContains(self.game.description,"id Software")
-
-    def test_game_genre(self):
-        self.assertEquals(self.game.genre.count(), 0)
-        self.game.fetch_genre()
-        self.assertEquals(self.game.genre.count(), 1)        
-        self.assertIn(self.genre, self.game.genre)
-
-    def test_gaming_index(self):        
-        response = self.client.get(reverse('index'))
-        self.assertIs(response.status_code, 200)
-        self.assertContains(response, self.game.name)
+    fixtures = ['testdata.json'] 
 
     def test_game_details(self):
-        url = reverse('detail', args=(self.game.id,))
+        url = reverse('detail', args=(2,))
         response = self.client.get(url)
         self.assertIs(response.status_code, 200)
-        self.assertContains(response, "Played")
+        self.assertContains(response, "Crash Bandicoot")
 
     def test_games_unplayed(self):        
         response = self.client.get(reverse('games_unplayed'))
         self.assertIs(response.status_code, 200)
-        self.assertNotContains(response, self.game.name)
-
-    def test_games_unfinished(self):        
-        response = self.client.get(reverse('games_unfinished'))
-        self.assertIs(response.status_code, 200)
-        self.assertContains(response, self.game.name)
+        self.assertContains(response, "Aliens Fireteam Elite")
 
     def test_gamesystems(self):        
         response = self.client.get(reverse('gamesystems'))
         self.assertIs(response.status_code, 200)
-        self.assertContains(response, self.gamesystem.name)
+        self.assertContains(response, "SNES")
 
     def test_gamesystem_details(self):
-        url = reverse('gamesystem_detail', args=(self.gamesystem.id,))
+        url = reverse('gamesystem_detail', args=(13,))
         response = self.client.get(url)
         self.assertIs(response.status_code, 200)
-        self.assertContains(response, self.game.name)
+        self.assertContains(response, "Game Boy")
 
     def test_gaming_search(self):
-        response = self.client.post(reverse('search'), data={"search_string": "test"})
+        response = self.client.post(reverse('search'), data={"search_string": "Crash"})
         self.assertIs(response.status_code, 200)
-        self.assertContains(response, self.game.name)
+        self.assertContains(response, "Crash Bandicoot")
 
