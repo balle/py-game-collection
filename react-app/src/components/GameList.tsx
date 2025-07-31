959056e49d7b82ac878356dee981c54208764eba
Bastian Ballmann <balle@codekid.net>
2025-07-31 13:42:01 +0200

    error handling

--- react-app/src/components/GameList.tsx
+++ react-app/src/components/GameList.tsx
@@ -24,34 +24,36 @@ function GameList({
   updateGames,
 }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [error, setError] = useState("");
 
   // TODO: need new api call for filtering
   useEffect(() => {
     const apiUrl = `${base_url}/api/games/?page=${pageNumber}`;
 
     let ajax = new XMLHttpRequest();
+    ajax.open("GET", apiUrl);
+    ajax.responseType = "json";
+    ajax.timeout = 10000;
     ajax.onload = function () {
-      try {
-        const parsed = JSON.parse(this.response);
-        const newGames: GameType[] = parsed["results"].map(
+      if (ajax.status == 200) {
+        const newGames: GameType[] = this.response["results"].map(
           (game: GameType) => ({
             id: game.id,
             name: game.name,
           })
         );
         updateGames(newGames);
-      } catch (error: any) {
-        console.error("Failed parsing /api/games response ", error.message);
+      } else {
+        setError("Failed fetching games");
       }
     };
 
-    ajax.open("GET", apiUrl, false);
-    ajax.setRequestHeader("Accept", "application/json");
     ajax.send();
   }, [pageNumber, selectedGenre, selectedGamesystem]);
 
   return (
     <>
+      {error && <p className="text-danger">{error}</p>}
       {games.length === 0 && <p>No games found</p>}
 
       <ul className="list-group">
